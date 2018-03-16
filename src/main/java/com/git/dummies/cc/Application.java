e7877b7d7d363755615a89bb790ed7665dd922d7
cosminc <cosmin.calistru@argus-presse.fr>
2018-03-16 15:55:39 +0100

    [INIT] Version 1

---
+++ src/main/java/com/git/dummies/cc/Application.java
@@ -0,0 +1,16 @@
+package com.git.dummies.cc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+
+    public static void main (String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("git");
+        words.add("for");
+        words.add("dummies");
+
+        System.out.println(words);
+    }
+}
