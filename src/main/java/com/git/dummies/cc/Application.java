0a9188345b9f7636290c6ded1f213bb548dce18b
cosminc <cosmin.calistru@argus-presse.fr>
2018-03-20 10:29:38 +0100

    Capitalize Git

--- src/main/java/com/git/dummies/cc/Application.java
+++ src/main/java/com/git/dummies/cc/Application.java
@@ -7,7 +7,7 @@ public class Application {
 
     public static void main (String[] args) {
         List<String> words = new ArrayList<>();
-        words.add("git");
+        words.add("Git");
         words.add("for");
         words.add("dummies");
 
